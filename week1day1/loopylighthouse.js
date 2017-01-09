5d4eefa7b855947351f9ca78dfec2ef3049da172
Roger Chao <roger.ti.chao@gmail.com>
2017-01-09 22:24:59 +0000

    loopy lighthouse

---
+++ week1day1/loopylighthouse.js
@@ -0,0 +1,15 @@
+for (var i = 100; i <= 200; i++){i
+
+  if (i % 12 === 0) {
+    console.log("LoopyLighthouse");
+  }
+  else if ( i % 3 === 0) { 
+    console.log("Loopy");
+  }
+  else if ( i % 4 === 0) {
+    console.log("Lighthouse");
+  }
+  else {
+    console.log(i);
+  }                                          
+}
--- week1day1/pig-latin.js
+++ week1day1/pig-latin.js
@@ -3,11 +3,16 @@ var arguments = process.argv.slice(2);
 function pigLatinify(words) {
   var pigLatinString = "";
 
+  var pigLatinArray = [];
+
   for(var i = 0; i < words.length; i++) {
 
     pigLatinString += words[i].slice(1, words[i].length) + words[i].charAt(0) + "ay ";
+
+    pigLatinArray.push(words[i].slice(1, words[i].length) + words[i].charAt(0) + "ay");
   }
-  return pigLatinString;
+  var str = pigLatinArray.join(" ");
+  return str;
 }
 
 console.log(pigLatinify(arguments));
