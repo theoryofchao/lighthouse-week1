157605bae35161e2fe340f70a33afd11ea8ebb16
Roger Chao <roger.ti.chao@gmail.com>
2017-01-11 21:39:08 +0000

    Found Waldo using a forEach loop

--- week1day4/findwaldo.js
+++ week1day4/findwaldo.js
@@ -7,8 +7,16 @@ function findWaldo(arr, found) {
   }
 }
 
+function findWaldo2(arr, found) {
+  arr.forEach(function(element, index) {
+    if (element === "Waldo") {
+      found(index);
+    }
+  });
+}
+
 function actionWhenFound(index) {
     console.log("Found him at index",index+"!");
 }
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+findWaldo2(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
