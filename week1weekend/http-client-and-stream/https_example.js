25a0f68c0e76794294cce67232b3145300ac1478
Roger Chao <roger.ti.chao@gmail.com>
2017-01-13 23:00:02 +0000

    finished http client and streams weekend exercise

--- week1weekend/http-client-and-stream/https_example.js
+++ week1weekend/http-client-and-stream/https_example.js
@@ -1,15 +1,20 @@
 var https = require(`https`);
 
 var options = {
-    host: `www.mangastream.com`,
-    path: `/`
+    host: `stream-large-file.herokuapp.com`,
+    path: `/give-me-stuff-now`
 };
 
 // called by https when the request is made
 var callback = function(response) {
     console.log(`In response handler callback!`);
-    console.log(`Response: ${response}`);
-    console.log(response);
+
+    //console.log(`Response: ${response}`);
+
+    response.on(`data`, function(chunk) {
+      console.log(`[-- CHUNK OF LENGTH ${chunk.length} --]`);
+      console.log(chunk.toString());
+    });
 };
 
 console.log(`I'm about to make the request!`);
